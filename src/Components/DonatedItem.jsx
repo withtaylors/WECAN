f723be8da819abb6f8c374c1190165e2f66dbb51
withtaylors <withtaylors@naver.com>
2024-01-01 22:32:38 +0900

    [UPDATE] DonatedItem Component 추가

--- src/Components/DonatedItem.jsx
+++ src/Components/DonatedItem.jsx
@@ -1,15 +1,15 @@
 import { Link } from "react-router-dom";
-import Card from "./Card";
+import Card3 from "./Card3";
 import styles from "./Styled/DonatedItem.module.css";
 
 function DonatedItem({ donate }) {
   return (
-    <Card className={styles.donateItem}>
+    <Card3 className={styles.donateItem}>
       <div className={styles.thumb}></div>
       <div className={styles.content}>
         <h2 className={styles.title}></h2>
       </div>
-    </Card>
+    </Card3>
   );
 }
 
