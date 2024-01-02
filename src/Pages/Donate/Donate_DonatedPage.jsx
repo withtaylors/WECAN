5e16075f56838cbb7b891c5d71d0c205bc3c97b3
withtaylors <withtaylors@naver.com>
2024-01-03 00:14:18 +0900

    [UPDATE] DonatedPage 백엔드 axios 코드 작성 + collapse 컴포넌트 시도

--- src/Pages/Donate/Donate_DonatedPage.jsx
+++ src/Pages/Donate/Donate_DonatedPage.jsx
@@ -6,12 +6,43 @@ import Warn from "../../Components/Warn";
 import DonatedItem from "../../Components/DonatedItem";
 import { getDonated } from "../../Api/getter";
 import TopNav from "../../Pages/TopNav/TopNav.main";
+import Collapse from "react-bootstrap/Collapse";
+import Button from "react-bootstrap/Button";
+
+import axios from "axios";
 
 function Donate_DonatedPage() {
   const [searchParam, setSearchParam] = useSearchParams();
   const initKeyword = searchParam.get("keyword");
   const [keyword, setKeyword] = useState(initKeyword || "");
-  const donated = getDonated(initKeyword); // mock.json에서 가져온 데이터
+  const [donated, setDonated] = useState([]); // 상태를 배열로 초기화
+  const [loading, setLoading] = useState(true); // 로딩 상태 관리
+  const [error, setError] = useState(null); // 에러 상태 관리
+  // Collapse 상태 관리를 위한 state 추가
+  const [open, setOpen] = useState(false);
+
+  // useEffect(() => {
+  //   // 백엔드에서 기부 데이터 가져오기
+  //   const fetchDonated = async () => {
+  //     try {
+  //       const response = await axios.get(`/api/donated?keyword=${keyword}`);
+  //       setDonated(response.data); // 가져온 데이터로 상태 업데이트
+  //       setLoading(false); // 로딩 상태를 false로 설정
+  //     } catch (err) {
+  //       setError(err); // 에러 발생 시 에러 상태 업데이트
+  //       setLoading(false); // 로딩 상태를 false로 설정
+  //     }
+  //   };
+  //   fetchDonated();
+  // }, [keyword]); // keyword가 변경될 때마다 API 호출
+
+  // if (loading) {
+  //   return <div>Loading...</div>; // 로딩 중일 때 표시될 요소
+  // }
+
+  // if (error) {
+  //   return <div>Error: {error.message}</div>; // 에러가 발생했을 때 표시될 요소
+  // }
 
   return (
     <>
@@ -20,12 +51,26 @@ function Donate_DonatedPage() {
       </div>
       <ListPage variant="catalog" title="기부했어요">
         <div className={styles.infoBar}>
-          <p>가나다순</p>
+          <p>최신순</p>
         </div>
 
         <div className={styles.donateList}>
-          {donated.map((donate) => (
-            <DonatedItem key={donated.id} />
+          {donated.map((item) => (
+            <div key={item.id}>
+              {/* 클릭 가능한 요소를 추가하여 Collapse 컴포넌트를 토글할 수 있게 합니다 */}
+              <Button
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+              >
+                {open ? "숨기기 ^" : "자세히 보기 v"}
+              </Button>
+              <Collapse in={open}>
+                <div id="example-collapse-text">
+                  <DonatedItem donate={item} />
+                </div>
+              </Collapse>
+            </div>
           ))}
         </div>
       </ListPage>
