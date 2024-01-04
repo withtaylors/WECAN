f90ede3c56bd4ed3741f22a0c9a162e44904df88
withtaylors <withtaylors@naver.com>
2024-01-05 00:15:14 +0900

    [UPDATE] 기부단체이미지추가 + Nav hover 효과 수정

--- src/Pages/TopNav/Styled/TopNav.style.js
+++ src/Pages/TopNav/Styled/TopNav.style.js
@@ -1,7 +1,7 @@
-import styled, { css } from 'styled-components';
-import { Link } from 'react-router-dom';
+import styled, { css } from "styled-components";
+import { Link } from "react-router-dom";
 
-export const activeClassName = 'active';
+export const activeClassName = "active";
 
 export const activeStyle = css`
   color: red;
@@ -12,17 +12,19 @@ export const ActiveLink = styled(Link).attrs({
 })`
   text-decoration: none;
   color: black;
+  border-bottom: 4px solid transparent;
+  padding-bottom: 27px;
 
   &:hover {
     color: #dd518c;
-    height: 70px;
-    border-bottom: 4px solid black;
-   
+    border-bottom-color: black;
+  }
 
   &.${activeClassName} {
     ${activeStyle}
   }
 `;
+
 export const ActiveLink2 = styled(Link).attrs({
   activeClassName,
 })`
@@ -40,7 +42,7 @@ export const ActiveLink2 = styled(Link).attrs({
 `;
 export const NavWrapperTotal = styled.div`
   margin-bottom: 20px;
-  margin-top: 40px;
+  margin-top: 60px;
 `;
 
 export const NavWrapper = styled.div`
@@ -49,7 +51,7 @@ export const NavWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  height: 70px;
+  height: 50px;
   width: 896px;
   z-index: 50;
   gap: 10px;
@@ -66,7 +68,7 @@ export const NavItem = styled.div`
   margin-right: 40px;
   font-size: 17px;
   font-weight: bold;
-  height: 70px;
+  height: 50px;
   width: 100px;
   display: flex;
   justify-content: center;
@@ -74,12 +76,12 @@ export const NavItem = styled.div`
 `;
 
 export const Submenu = styled.div`
-  display: ${(props) => (props.show ? 'flex' : 'none')};
+  display: ${(props) => (props.show ? "flex" : "none")};
   position: absolute;
   top: 100%;
   left: 0;
   width: max-content;
-  height: 60px;
+  height: 50px;
   justify-content: center;
   align-items: center;
   flex-direction: row;
@@ -92,95 +94,3 @@ export const SubmenuItem = styled.div`
   margin-right: 50px;
   font-size: 16px;
 `;
-
-////
-// const activeClassName = "active";
-
-// const activeStyle = css`
-//   color: red;
-// `;
-// const ActiveLink = styled(Link).attrs({
-//   activeClassName,
-// })`
-//   text-decoration: none;
-//   color: black;
-//   font-weight: bold;
-//   width: 140px;
-//   height: 85px;
-//   display: flex;
-//   align-items: center;
-//   justify-content: center;
-
-//   &:hover {
-//     color: #dd518c;
-//     border-bottom: 4px solid black;
-//   }
-
-//   &.${activeClassName} {
-//     ${activeStyle}
-//   }
-// `;
-// const ActiveLink2 = styled(Link).attrs({
-//   activeClassName,
-// })`
-//   text-decoration: none;
-//   color: black;
-//   font-weight: bold;
-
-//   &:hover {
-//     color: #dd518c;
-//   }
-
-//   &.${activeClassName} {
-//     ${activeStyle}
-//   }
-// `;
-// const NavWrapperTotal = styled.div`
-//   margin-bottom: 20px;
-//   margin-top: 55px;
-// `;
-// const NavWrapper = styled.div`
-//   display: flex;
-//   flex-direction: row;
-//   align-items: center;
-//   justify-content: center;
-//   height: 65px;
-//   width: 896px;
-//   z-index: 50;
-//   gap: 20px;
-//   border-bottom: 2px solid gray;
-// `;
-// const Divider = styled.div`
-//   width: 100%;
-//   height: 10px;
-//   background-color: gray;
-// `;
-
-// const NavItem = styled.div`
-//   position: relative;
-//   cursor: pointer;
-//   margin-bottom: 10px;
-//   margin-left: 40px;
-//   margin-right: 40px;
-//   font-size: 24px;
-//   height: 80px;
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const SubmenuItem = styled.div`
-//   padding: 5px;
-//   color: white;
-//   cursor: pointer;
-//   margin-right: 50px;
-//   font-size: 17px;
-// `;
-// const Submenu = styled.div`
-//   display: ${(props) => (props.show ? "flex" : "none")};
-//   position: absolute;
-//   top: 100%;
-//   left: 0;
-//   width: 1000px;
-//   heigt: 100px;
-//   flex-direction: row;
-// `;
