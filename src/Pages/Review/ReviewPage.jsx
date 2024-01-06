3d8304dc6705685c22dc79345ed2a354f7c4f719
withtaylors <withtaylors@naver.com>
2024-01-06 15:43:23 +0900

    [UPDATE] ReviewPage 구현중

--- src/Pages/Review/ReviewPage.jsx
+++ src/Pages/Review/ReviewPage.jsx
@@ -6,19 +6,64 @@ import Warn from "../../Components/Warn";
 import DonatedItem from "../../Components/DonatedItem";
 import { getDonated } from "../../Api/getter";
 import TopNav from "../../Pages/TopNav/TopNav.main";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import styled from "@emotion/styled";
+import SingleReview from "../../Components/SingleReview"; // Corrected typo here
 
-function ReviewPage() {
-  return (
-    <>
-      <div className={styles.center}>
-        <TopNav></TopNav>
-      </div>
-    </>
-  );
+class ReviewView extends React.Component {
+  state = {
+    reviews: [
+      {
+        id: 1,
+        writer: "일일일",
+        date: "2020-10-10",
+        content: "후기입니다.",
+      },
+      {
+        id: 2,
+        writer: "둘둘둘",
+        date: "2020-10-09",
+        content: "후기입니다.\n 두번째 줄입니다. \n 세번째 줄입니다.",
+      },
+    ],
+  };
+
+  addReview = () => {
+    let value = document.querySelector("#new-review-content").value;
+    this.setState({
+      reviews: [
+        ...this.state.reviews,
+        {
+          id: this.state.reviews.length + 1,
+          writer: "김코딩",
+          date: new Date().toISOString().slice(0, 10),
+          content: value,
+        },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <div className={styles.center}>
+          <TopNav></TopNav>
+        </div>
+        <div>
+          <div>작성자 : 김코딩</div>
+          <div id="writing-area">
+            <textarea id="new-review-content"></textarea>
+            <button id="submit-new-review" onClick={this.addReview}>
+              새 댓글 쓰기
+            </button>
+          </div>
+          <ul id="review" className={styles.bubble_left}>
+            {this.state.reviews.map((review) => (
+              <SingleReview key={review.id} review={review} />
+            ))}
+          </ul>
+        </div>
+      </>
+    );
+  }
 }
 
-export default ReviewPage;
+export default ReviewView;
