80cddf74d71123c21e2d3b17933fd7ac574242d1
withtaylors <withtaylors@naver.com>
2024-01-20 21:54:29 +0900

    [update] 기부했어요 페이지 수정중

--- src/Components/Card3.jsx
+++ src/Components/Card3.jsx
@@ -1,8 +1,44 @@
-import classNames from "classnames";
-import styles from "./Styled/Card.module.css";
+// Card3.jsx
+import classNames from 'classnames';
+import { useState } from 'react';
+import styles from './Styled/Card3.module.css'; // 경로가 올바른지 확인
+import icoArrowDown from '../Assets/img/donated/arrow.png';
 
-function Card3({ className, children }) {
-  return <div className={classNames(styles.card, className)}>{children}</div>;
+function Card3({ title, content, imageSrc }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const contentLines = content.split('\n'); // \n을 기준으로 문자열을 분리합니다.
+
+  return (
+    <div className={styles.card}>
+      <div className={styles.cardHeader} onClick={() => setIsOpen(!isOpen)}>
+        <img
+          className={classNames(styles.thumb, styles.center)}
+          src={imageSrc}
+          alt={title}
+        />
+        <div className={styles.titleArrowWrapper}>
+          <h2 className={styles.title}>{title}</h2>
+          <img
+            className={classNames(styles.arrow, {
+              [styles.rotateArrow]: isOpen,
+            })}
+            src={icoArrowDown}
+            alt="상세 정보 토글"
+          />
+        </div>
+      </div>
+      <div
+        className={classNames(styles.cardContent, {
+          [styles.cardContentOpen]: isOpen,
+        })}
+      >
+        {isOpen &&
+          contentLines.map((line, index) => (
+            <p key={index}>{line}</p> // 각 줄을 <p> 태그로 감쌉니다.
+          ))}
+      </div>{' '}
+    </div>
+  );
 }
 
 export default Card3;
--- src/Pages/Donate/Donate_DonatedPage.jsx
+++ src/Pages/Donate/Donate_DonatedPage.jsx
@@ -1,44 +1,60 @@
-import React, { useState, useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
-import DonateListPage from "./DonateListPage";
-import styles from "./Styled/Donate_DonatedPage.module.css";
-import Warn from "../../Components/Warn";
-import DonatedItem from "../../Components/DonatedItem";
-import { getDonated } from "../../Api/getter";
-import TopNav from "../../Pages/TopNav/TopNav.main";
-import Collapse from "react-bootstrap/Collapse";
-import Button from "react-bootstrap/Button";
-import CharityItem from "../../Components/CharityItem";
-import { getCharitys } from "../../Api/getter";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import DonateListPage from './DonateListPage';
+import styles from './Styled/Donate_DonatedPage.module.css';
+import Warn from '../../Components/Warn';
+import DonatedItem from '../../Components/DonatedItem';
+import { getDonated } from '../../Api/getter';
+import TopNav from '../../Pages/TopNav/TopNav.main';
+import Collapse from 'react-bootstrap/Collapse';
+import Button from 'react-bootstrap/Button';
+import CharityItem from '../../Components/CharityItem';
+import { getCharitys } from '../../Api/getter';
+import axios from 'axios';
+import donated from '../../Api/donated.json';
 
 function Donate_DonatedPage() {
   const [searchParam, setSearchParam] = useSearchParams();
-  const initKeyword = searchParam.get("keyword");
-  const [keyword, setKeyword] = useState(initKeyword || "");
-  // useState를 사용하여 charitys 상태와 그 상태를 업데이트하는 함수를 정의
+  const initKeyword = searchParam.get('keyword');
+  const [keyword, setKeyword] = useState(initKeyword || '');
   const charitys = getCharitys(initKeyword); // mock.json에서 가져온 데이터
+  const [images, setImages] = useState({});
+
+  useEffect(() => {
+    // 이미지를 동적으로 불러오는 로직
+    donated.forEach((item) => {
+      import(`../../Assets/img/donated/${item.imageSrc}`)
+        .then((image) => {
+          setImages((prevImages) => ({
+            ...prevImages,
+            [item.id]: image.default,
+          }));
+        })
+        .catch((error) => {
+          console.error('이미지 로딩 오류:', error);
+        });
+    });
+  }, []);
 
   return (
     <>
       <div className={styles.center}>
         <TopNav></TopNav>
       </div>
-
- <DonateListPage variant="catalog" title="기부했어요">
+      <DonateListPage variant="catalog" title="기부했어요">
         <div className={styles.infoBar}>
           <p>최신순</p>
         </div>
         <div className={styles.charityList}>
-          {charitys.map((charity) => {
-            return (
-              <DonatedItem
-                key={charity.id}
-                challengeName={charity.challengeName} // Pass challengeName as prop
-                explanation={charity.explanation} // Pass explanation as prop
-              />
-            );
-          })}
+          {donated.map((charity) => (
+            <DonatedItem
+              key={charity.id}
+              id={charity.id}
+              challengeName={charity.challengeName}
+              explanation={charity.explanation}
+              imageSrc={images[charity.id]}
+            />
+          ))}
         </div>
       </DonateListPage>
     </>
