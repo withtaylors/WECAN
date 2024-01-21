e13008dcdedda0d07e3b443190746e37a5a540f4
withtaylors <withtaylors@naver.com>
2024-01-21 18:06:24 +0900

    [save] 중간지점 저장

--- src/Pages/Chat/ChatPage.jsx
+++ src/Pages/Chat/ChatPage.jsx
@@ -1,75 +1,111 @@
 import React, { useState, useEffect } from 'react';
+import { Client } from '@stomp/stompjs';
 import Calendar from 'react-calendar';
-import { ChatList } from 'react-chat-elements';
 import 'react-calendar/dist/Calendar.css';
-import 'react-chat-elements/dist/main.css';
-import './Styled/ChatPage.module.css';
-import chatdata from '../../Api/chat.json'; // JSON 파일을 import
-import Calendar2 from 'react-calendar';
-import 'react-calendar/dist/Calendar.css'; // css import
-import moment from 'moment';
-import styled from 'styled-components';
-import selectArrow from '../../Assets/search.svg';
+import styles from './Styled/ChatPage.module.css';
 
-const CalendarContainer = styled.div`
-  position: relative;
-`;
-
-const DropdownButton = styled.button`
-  width: 200px;
-  height: 48px;
-  border: 0.8px solid var(--festie-gray-600, #949494);
-  border-radius: 10px;
-  padding: 0px 12px;
-  color: var(--festie-gray-800, #3a3a3a);
-  font-family: SUIT Variable;
-  font-size: 16px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 140%;
-  text-align: start;
-  appearance: none;
-  background-color: white;
-  background-image: url(${selectArrow});
-  background-repeat: no-repeat;
-  background-position: right 12px center;
-  background-size: 12px;
-`;
-
-const CalendarWrapper = styled.div`
-  z-index: 11;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  display: ${(props) => (props.isOpen ? 'block' : 'none')};
-`;
-
-const CustomCalendar = ({ onChange, value }) => {
-  const [nowDate, setNowDate] = useState('날짜');
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleToggleCalendar = () => {
-    setIsOpen(!isOpen);
-  };
-
-  const handleDateChange = (selectedDate) => {
-    onChange(selectedDate);
-    setIsOpen(false);
-    setNowDate(moment(selectedDate).format('YYYY년 MM월 DD일'));
-  };
-
-  return (
-    <CalendarContainer>
-      <DropdownButton onClick={handleToggleCalendar}>{nowDate}</DropdownButton>
-      <CalendarWrapper isOpen={isOpen}>
-        <Calendar2
-          onChange={handleDateChange}
-          value={value}
-          formatDay={(locale, date) => moment(date).format('DD')}
-        ></Calendar2>
-      </CalendarWrapper>
-    </CalendarContainer>
-  );
+const ChatPage = ({ userId, userNickName, onDateSelect }) => {
+  //   const today = new Date();
+  //   today.setHours(23, 59, 59, 999);
+  //   const [data, setData] = useState({
+  //     title: '',
+  //     startDate: '',
+  //     endDate: '',
+  //     successRate: '',
+  //     chattingRoomId: null,
+  //     chattingList: [],
+  //   });
+  //   const handleDateChange = (newDate) => {
+  //     setSelectedDate(newDate);
+  //     if (typeof onDateSelect === 'function') {
+  //       onDateSelect(newDate, setData);
+  //     } else {
+  //       console.error('handleondateSelect 오류');
+  //     }
+  //   };
+  //   const parsedStartDate =
+  //     data && data.startDate ? new Date(data.startDate) : new Date();
+  //   const parsedEndDate =
+  //     data && data.endDate ? new Date(data.endDate) : new Date();
+  // const [stompClient, setStompClient] = useState(null);
+  //   useEffect(() => {
+  //     // WebSocket 및 STOMP 설정
+  //     const websocket = new WebSocket("ws://3.35.3.205:8080/ws");
+  //     const stomp = new Client({
+  //       webSocketFactory: () => websocket
+  //     });
+  //     stomp.activate();
+  //     stomp.onConnect = () => {
+  //       console.log("STOMP Connection");
+  //       // 채팅방 구독
+  //       stomp.subscribe("/sub/chat/room/" + data.chattingRoomId, (chat) => {
+  //         const content = JSON.parse(chat.body);
+  //         setMessages(prev => [...prev, content]);
+  //       });
+  //     };
+  //     setStompClient(stomp);
+  //     return () => {
+  //       stomp.deactivate();
+  //     };
+  //   }, [data.chattingRoomId]);
+  //   const sendMessage = () => {
+  //     if (stompClient && inputMessage) {
+  //       const now = new Date();
+  //       const currentTime = now.toISOString();
+  //       const chatMessage = {
+  //         type: 'TALK',
+  //         roomId: data.chattingRoomId,
+  //         userId: userId,
+  //         nickName: userNickName,
+  //         message: inputMessage,
+  //         time: currentTime,
+  //       };
+  //       stompClient.publish({
+  //         destination: '/pub/sendMessage',
+  //         headers: {},
+  //         body: JSON.stringify(chatMessage),
+  //       });
+  //       console.log('Message sent:', chatMessage);
+  //       setInputMessage('');
+  //     }
+  //   };
+  //   return (
+  //     <div>
+  //       <div className={styles.calendarContainer}>
+  //         <Calendar
+  //           onChange={handleDateChange}
+  //           value={selectedDate}
+  //           minDate={new Date(data.startDate)}
+  //           maxDate={today}
+  //         />
+  //       </div>
+  //       <div className="chat-room">
+  //         <h2>{data.title}</h2>
+  //         <p>Start Date: {data.startDate}</p>
+  //         <p>End Date: {data.endDate}</p>
+  //         <p>Success Rate: {data.successRate}</p>
+  //         <div className="messages-container">
+  //           {messages.map((msg, index) => (
+  //             <div
+  //               key={index}
+  //               className={`message ${msg.userId === userId ? 'mine' : ''}`}
+  //             >
+  //               <span className="message-nickName">{msg.nickName}</span>:{' '}
+  //               {msg.message}
+  //             </div>
+  //           ))}
+  //         </div>
+  //         <div className="input-area">
+  //           <input
+  //             type="text"
+  //             value={inputMessage}
+  //             onChange={(e) => setInputMessage(e.target.value)}
+  //           />
+  //           <button onClick={sendMessage}>Send</button>
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
 };
 
-export default CustomCalendar;
+export default ChatPage;
