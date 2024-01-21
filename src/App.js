bd99774e2d1f47d9ae9d26d516dd79b39f3cb9ab
youjae <youjae0803@gmail.com>
2024-01-21 19:50:36 +0900

    feat: kakao login

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import {
   Routes,
   Route,
   useLocation,
-} from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+} from "react-router-dom";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import './App.css';
-import './App.font.css';
+import "./App.css";
+import "./App.font.css";
 
-import TopNav from './Pages/TopNav/Top.main';
-import Footer from './Components/Footer';
-import Home from './Pages/Home/Home.main';
-import Login from './Pages/Login/Login.main';
-import Join from './Pages/Join/Join.main';
-import Recruit from './Pages/Recruit/Recruit.total.main';
-import Donate_CharityPage from './Pages/Donate/Donate_CharityPage';
-import Donate_CharityListPage from './Pages/Donate/Donate_CharityListPage';
-import Donate_DonatedPage from './Pages/Donate/Donate_DonatedPage';
-import ReviewPage from './Pages/Review/ReviewPage';
-import Shop_MainPage from './Pages/Shop/Shop_MainPage';
-import Shop_DetailPage from './Pages/Shop/Shop_DetailPage';
-import NotFoundPage from './Pages/NotFoundPage';
-import ChallengeInfo from './Pages/Challenger/Challenger.main';
-import ChallengeMakeMain from './Pages/ChallengeMake/ChallengeMake.main';
-import ChatPage from './Pages/Chat/ChatPage';
-import Mypage from './Pages/MyPage/Mypage.main';
-import InfoChange from './Pages/MyPage/InfoChange/InfoChange.main';
-import { Info } from './Pages/Challenger/Styled/Challenger.review.main';
+import TopNav from "./Pages/TopNav/Top.main";
+import Footer from "./Components/Footer";
+import Home from "./Pages/Home/Home.main";
+import Login from "./Pages/Login/Login.main";
+import Join from "./Pages/Join/Join.main";
+import Recruit from "./Pages/Recruit/Recruit.total.main";
+import Donate_CharityPage from "./Pages/Donate/Donate_CharityPage";
+import Donate_CharityListPage from "./Pages/Donate/Donate_CharityListPage";
+import Donate_DonatedPage from "./Pages/Donate/Donate_DonatedPage";
+import ReviewPage from "./Pages/Review/ReviewPage";
+import Shop_MainPage from "./Pages/Shop/Shop_MainPage";
+import Shop_DetailPage from "./Pages/Shop/Shop_DetailPage";
+import NotFoundPage from "./Pages/NotFoundPage";
+import ChallengeInfo from "./Pages/Challenger/Challenger.main";
+import ChallengeMakeMain from "./Pages/ChallengeMake/ChallengeMake.main";
+import ChatPage from "./Pages/Chat/ChatPage";
+import Mypage from "./Pages/MyPage/Mypage.main";
+import InfoChange from "./Pages/MyPage/InfoChange/InfoChange.main";
+import KakaoRedirect from "./Pages/Login/KakaoRedirect.main";
+import { Info } from "./Pages/Challenger/Styled/Challenger.review.main";
 
 const queryClient = new QueryClient();
 
@@ -74,6 +75,10 @@ function App() {
                 />
                 <Route path="/chat" element={<ChatPage />} />
                 <Route path="*" element={<NotFoundPage />} />
+                <Route
+                  path="/login/oauth/kakao"
+                  element={<KakaoRedirect />}
+                ></Route>
               </Routes>
             </div>
             <ConditionalFooter />
@@ -88,7 +93,7 @@ function Navigation() {
   let location = useLocation();
 
   // 로그인과 회원가입 경로에서 TopNav를 숨김
-  if (location.pathname === '/login' || location.pathname === '/join') {
+  if (location.pathname === "/login" || location.pathname === "/join") {
     return null;
   }
 
@@ -99,7 +104,7 @@ function ConditionalFooter() {
   let location = useLocation();
 
   // 로그인과 회원가입 경로에서 Footer를 숨김
-  if (location.pathname === '/login' || location.pathname === '/join') {
+  if (location.pathname === "/login" || location.pathname === "/join") {
     return null;
   }
 
