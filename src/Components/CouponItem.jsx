1c42d35be8781a88b7f2999f481c33cfa33c414c
withtaylors <withtaylors@naver.com>
2024-01-23 02:32:40 +0900

    [update] 굿즈샵 틀 완성

--- src/Components/CouponItem.jsx
+++ src/Components/CouponItem.jsx
@@ -1,12 +1,18 @@
 import { Link } from 'react-router-dom';
 import Card_Shop from './Card_Shop';
 import styles from './Styled/ShopItem.module.css';
+import CouponIcon from '../Components/CouponIcon';
 
 function CouponItem({ coupon }) {
   return (
     <Card_Shop className={styles.shopItem}>
       <Link to={`/coupon/${coupon.slug}`}>
-        <div className={styles.thumb}></div>
+        <div className={styles.thumb}>
+          <CouponIcon
+            photoUrl={coupon.photoUrl}
+            className={styles.charityIcon}
+          />
+        </div>
         <div className={styles.content}>
           <h2 className={styles.title}>{coupon.title}</h2>
         </div>
